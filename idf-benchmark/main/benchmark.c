3cb168cf2600177dd3798b7a9644c1b10aeb878e
martinberlin <martin@luckycloud.de>
2019-10-14 08:35:10 +0200

    Reorganize structure

--- idf-benchmark/main/benchmark.c
+++ idf-benchmark/main/benchmark.c
@@ -13,129 +13,17 @@
 #include "esp_timer.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
-
-/* Can run 'make menuconfig' to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
-
-static const char* TAG = "benchmark";
-
+#include "benchtests.c"
 
 void app_main()
 {
 
-  int64_t d, overhead;
-  int64_t time_since_boot = esp_timer_get_time();
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. 
-    */
-    gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    printf("Speed test\n");
-    printf("----------\n");
-    printf("GPIO set test\n");
-    gpio_set_level(BLINK_GPIO, 0);
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-    printf("digitalRead: \n");
+  blink_test();
 
-  int64_t start = esp_timer_get_time();
+  math_multiply();
 
-  for (int i=0; i<2; i++)
-  {
-    for (int j=0; j<10000; j++)
-    {
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-      gpio_get_level(BLINK_GPIO);       
-    }
-  }
-  int64_t after = esp_timer_get_time();
-  d = after-start;
-  d /= 10.0;
-
-  ESP_LOGI(TAG, "Boot time: %lld us", time_since_boot);
-  ESP_LOGI(TAG, "GPIO_get_level * 200000: %lld us (digitalRead)", d);
-
-  start = esp_timer_get_time();
-
-  for (int i=0; i<2; i++)
-  {
-    for (int j=0; j<10000; j++)
-    {
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0);   
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-      gpio_set_level(BLINK_GPIO, 1); 
-      gpio_set_level(BLINK_GPIO, 0); 
-    }
-  }
-  after = esp_timer_get_time();
-  d = after-start;
-  d /= 20.0;
-  ESP_LOGI(TAG, "GPIO_set_level ON/OFF * 200000: %lld us (digitalWrite)", d);
-
-
-  unsigned char c1,c2;
-  c1 = 2;
-  c2 = 3;
-start = esp_timer_get_time();
-
-  for (int i=0; i<20; i++)
-  {
-    for (int j=0; j<10000; j++)
-    {
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-      c1 *= c2;
-    }
-  }
-  after = esp_timer_get_time();
-  d = after-start;
-  d /= 20.0;
-  ESP_LOGI(TAG, "Multiply byte 200000*20: %lld us", d);
+  gpio_on_off();
+  
 
   printf("End of test ------------\n"); 
-  
 }
---
+++ idf-benchmark/main/benchtests.c
@@ -0,0 +1,130 @@
+
+/* Can run 'make menuconfig' to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+static const char* TAG = "benchmark";
+
+int64_t start;
+int64_t after;
+int64_t d;
+
+gpio_num_t gpio_blink = BLINK_GPIO;
+
+void blink_test() {
+  
+  int64_t time_since_boot = esp_timer_get_time();
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. 
+    */
+    gpio_pad_select_gpio(gpio_blink);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(gpio_blink, GPIO_MODE_OUTPUT);
+    printf("Speed test\n");
+    printf("----------\n");
+    printf("GPIO set test\n");
+    gpio_set_level(gpio_blink, 0);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    printf("digitalRead: \n");
+
+  start = esp_timer_get_time();
+
+  for (int i=0; i<2; i++)
+  {
+    for (int j=0; j<10000; j++)
+    {
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+      gpio_get_level(gpio_blink);       
+    }
+  }
+  after = esp_timer_get_time();
+  d = after-start;
+  d /= 10.0;
+
+  ESP_LOGI(TAG, "Boot time: %lld us", time_since_boot);
+  ESP_LOGI(TAG, "GPIO_get_level * 200000: %lld us (digitalRead)", d);
+}
+
+void gpio_on_off() {
+      start = esp_timer_get_time();
+
+  for (int i=0; i<2; i++)
+  {
+    for (int j=0; j<10000; j++)
+    {
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0);   
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+      gpio_set_level(gpio_blink, 1); 
+      gpio_set_level(gpio_blink, 0); 
+    }
+  }
+  after = esp_timer_get_time();
+  d = after-start;
+  d /= 20.0;
+  ESP_LOGI(TAG, "GPIO_set_level ON/OFF * 200000: %lld us (digitalWrite)", d);
+
+}
+
+void math_multiply() {
+     unsigned char c1,c2;
+  c1 = 2;
+  c2 = 3;
+start = esp_timer_get_time();
+
+  for (int i=0; i<20; i++)
+  {
+    for (int j=0; j<10000; j++)
+    {
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+      c1 *= c2;
+    }
+  }
+  after = esp_timer_get_time();
+  d = after-start;
+  d /= 20.0;
+  ESP_LOGI(TAG, "Multiply byte 200000*20: %lld us", d);
+}
